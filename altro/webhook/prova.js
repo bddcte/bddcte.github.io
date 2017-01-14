13bb0caa6765bd2476f6a571690cde44a2910808
mg <bddcte@gmail.com>
2017-01-14 20:19:03 +0100

    caricato

--- altro/webhook/prova.js
+++
@@ -1,7 +0,0 @@
-// facebook messenger webook
-app.get('/webhook/', function (req, res) {
-  if (req.query['hub.verify_token'] === 'prova') {
-    res.send(req.query['hub.challenge'])
-  }
-  res.send('Error, wrong token')
-})
